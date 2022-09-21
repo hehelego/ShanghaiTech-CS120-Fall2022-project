77a19f67f49f7558534aab603b962b1ea9b03948
hehelego <2364261262@qq.com>
2022-09-21 14:26:05 +0800

    UPD: sample_stream/loopback_stream change LoopBack to a tuple struct

--- proj1_acoustic_link/src/sample_stream/loopback_stream.rs
+++ proj1_acoustic_link/src/sample_stream/loopback_stream.rs
@@ -1,5 +1,40 @@
-use crate::block_buffer::ConcurrentBuffer;
+use crate::{
+  block_buffer::ConcurrentBuffer,
+  traits::{InStream, OutStream},
+};
 
 /// A loopback streae.
 /// The stream can read out whatever written into it.
-pub type LoopBackStream = ConcurrentBuffer<f32>;
+pub struct LoopBackStream(ConcurrentBuffer<f32>);
+
+impl LoopBackStream {
+  pub fn new() -> Self {
+    Self(ConcurrentBuffer::new())
+  }
+}
+
+impl Default for LoopBackStream {
+  fn default() -> Self {
+    Self::new()
+  }
+}
+
+impl InStream<f32, ()> for LoopBackStream {
+  fn read(&mut self, buf: &mut [f32]) -> Result<usize, ()> {
+    self.0.read(buf)
+  }
+
+  fn read_exact(&mut self, buf: &mut [f32]) -> Result<(), ()> {
+    self.0.read_exact(buf)
+  }
+}
+
+impl OutStream<f32, ()> for LoopBackStream {
+  fn write(&mut self, buf: &[f32]) -> Result<usize, ()> {
+    self.0.write(buf)
+  }
+
+  fn write_exact(&mut self, buf: &[f32]) -> Result<(), ()> {
+    self.0.write_exact(buf)
+  }
+}
