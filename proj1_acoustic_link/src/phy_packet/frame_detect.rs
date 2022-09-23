45d8621a76e46a5f31a6f01f0937e2b5abc6b977
hehelego <2364261262@qq.com>
2022-09-23 19:40:32 +0800

    UPD: phy_packet/frame_detect.rs combine Correlation&CosineSimilarity

--- proj1_acoustic_link/src/phy_packet/frame_detect.rs
+++ proj1_acoustic_link/src/phy_packet/frame_detect.rs
@@ -7,10 +7,16 @@ enum FramingState {
   WaitPayload,
 }
 
+/// If a preamble exists, the correlation pattern is:
+/// 1. first goes up quickly when the preamble starts
+/// 2. stay at plateau for a while
+/// 3. reduce quickly after the preamble ends
+/// Our approach is to detect **the falling edge**
 pub struct CorrelationFraming {
   preamble_len: usize,
   payload_len: usize,
   preamble: Vec<f32>,
+  preamble_norm: f32,
   state: FramingState,
   index: usize,
   stream_head: usize,
@@ -18,14 +24,24 @@ pub struct CorrelationFraming {
   peak_value: f32,
   peak_index: usize,
   detect_window: VecDeque<f32>,
+  detect_sqr_sum: f32,
   frame_window: VecDeque<f32>,
   stream_window: VecDeque<f32>,
 }
 
 impl CorrelationFraming {
-  pub const CORR_PWR_RATIO_THRESHOLD: f32 = 1.5;
-  pub const PWR_MIN: f32 = 0.05;
-  pub const AFTER_PEAK_SAMPLES: usize = 32;
+  /// For preamble detection,
+  /// the ratio of the correlation power to the received signal average power
+  /// must be greater than this threshold.  
+  /// Different threshold should be used for different media.  
+  /// - ideal transmission: 0.7
+  /// - air gapped transmission: 2.0
+  pub const CORR_TO_PWR_MIN: f32 = if cfg!(feature = "wired") { 0.7 } else { 2.0 };
+  /// Minimum cosine similarity required for preamble detection.
+  /// Used to skip irrelevant patterns with extremely high power.
+  pub const COSINE_MIN: f32 = 0.4;
+  /// The falling edge can be detected about 200 samples after the correlation peak appears.
+  pub const AFTER_PEAK_SAMPLES: usize = 200;
 
   fn stream_range(&self, start: usize, end: usize) -> Vec<f32> {
     let offset = self.stream_head;
@@ -36,21 +52,26 @@ impl CorrelationFraming {
   // stop when the Cosine-Similarity of detect window and the preamble is greater than a threshold
   fn iter_detect_preamble(&mut self, sample: f32) -> FramingState {
     // update the preamble detect window
-    self.detect_window.pop_front();
+    let head = self.detect_window.pop_front().unwrap();
     self.detect_window.push_back(sample);
+    self.detect_sqr_sum += sample * sample - head * head;
 
     // compute the correlation
-    let corr = 2.0 * dot_product(self.detect_window.iter(), self.preamble.iter()) / self.preamble_len as f32;
+    let dot = dot_product(self.detect_window.iter(), self.preamble.iter());
+    let corr2pwr = (dot / self.preamble_len as f32) / self.power;
+    let cosine_sim = dot / self.preamble_norm / self.detect_sqr_sum.sqrt();
 
     // - max correlation: end of the preamble sequence
     // - correlation drops down: payload section begins
     // - otherwise: skip
-    if corr > self.power * Self::CORR_PWR_RATIO_THRESHOLD && corr > self.peak_value.max(Self::PWR_MIN) {
-      self.peak_value = corr;
+    if corr2pwr > Self::CORR_TO_PWR_MIN.max(self.peak_value) && cosine_sim > Self::COSINE_MIN {
+      self.peak_value = corr2pwr;
       self.peak_index = self.index;
       FramingState::DetectPreamble
     } else if self.index - self.peak_index > Self::AFTER_PEAK_SAMPLES && self.peak_index != 0 {
       self.detect_window.iter_mut().for_each(|x| *x = 0.0);
+      self.detect_sqr_sum = 0.0;
+
       let frame_samples = self.stream_range(self.peak_index + 1, self.index);
       self.frame_window.extend(frame_samples);
 
@@ -106,17 +127,20 @@ impl FrameDetector for CorrelationFraming {
   fn new(preamble_sequence: Vec<f32>, payload_samples: usize) -> Self {
     let m = preamble_sequence.len();
     let n = m + payload_samples;
+    let sqr_sum = preamble_sequence.iter().fold(0.0, |s, &x| s + x * x);
     Self {
       preamble_len: m,
       payload_len: payload_samples,
       preamble: preamble_sequence,
+      preamble_norm: sqr_sum.sqrt(),
       state: FramingState::DetectPreamble,
       index: 0,
       stream_head: 0,
-      power: 0.0,
+      power: sqr_sum / m as f32,
       peak_value: 0.0,
       peak_index: 0,
       detect_window: std::iter::repeat(0.0).take(m).collect(),
+      detect_sqr_sum: 0.0,
       frame_window: VecDeque::with_capacity(n),
       stream_window: VecDeque::with_capacity(n * 2 + 1),
     }
