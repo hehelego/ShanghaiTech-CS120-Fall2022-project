f13c5f2d07a9639db63b76f6878aab832c84f831
hehelego <2364261262@qq.com>
2022-10-07 22:24:20 +0800

    UPD: tests/part4 send file with ECC

--- proj1_acoustic_link/tests/part4.rs
+++ proj1_acoustic_link/tests/part4.rs
@@ -1,5 +1,8 @@
 use crc::{Crc, CRC_64_ECMA_182};
-use proj1_acoustic_link::phy_layer::{ecc_recv, ecc_send};
+use proj1_acoustic_link::{
+  helper::bytes_to_bits,
+  phy_layer::{ecc_recv, ecc_send},
+};
 use rand::{distributions::Standard, Rng};
 
 fn checksum(bytes: &[u8]) -> u64 {
@@ -21,4 +24,41 @@ pub fn part4_recv() {
   let mut bytes: Vec<u8> = vec![0; 10000 / 8];
   ecc_recv(&mut bytes);
   println!("recv {:?}", checksum(&bytes));
+}
+
+#[test]
+#[ignore]
+pub fn part4_sendfile() {
+  let bits: Vec<u8> = std::fs::read_to_string("input.txt")
+    .unwrap()
+    .trim_end()
+    .chars()
+    .map(|c| match c {
+      '0' => 0,
+      '1' => 1,
+      _ => panic!("unexpected char in input"),
+    })
+    .collect();
+  assert_eq!(bits.len(), 10000);
+  let bytes = bytes_to_bits(&bits);
+  println!("send {:?}", checksum(&bytes));
+  ecc_send(&bytes);
+}
+
+#[test]
+#[ignore]
+pub fn part4_recvfile() {
+  let mut bytes: Vec<u8> = vec![0; 10000 / 8];
+  ecc_recv(&mut bytes);
+  println!("recv {:?}", checksum(&bytes));
+  let bits = bytes_to_bits(&bytes);
+  let bits: String = bits
+    .iter()
+    .map(|b| match b {
+      0 => '0',
+      1 => '1',
+      _ => panic!("unexpected bit"),
+    })
+    .collect();
+  std::fs::write("output.txt", bits).unwrap();
 }
