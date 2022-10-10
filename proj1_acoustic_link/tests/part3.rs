f974d43a525f5bd8fb1c84937606cc0da26b88c9
hehelego <2364261262@qq.com>
2022-10-10 22:04:54 +0800

    UPD: tests/part3.rs move helpers

--- proj1_acoustic_link/tests/part3.rs
+++ proj1_acoustic_link/tests/part3.rs
@@ -1,41 +1,9 @@
-use proj1_acoustic_link::helper::{bits_to_bytes, bytes_to_bits};
+use proj1_acoustic_link::helper::{add_padding, bits_to_bytes, bytes_to_bits, remove_padding};
 use proj1_acoustic_link::phy_layer::PlainPHY;
 use proj1_acoustic_link::traits::{PacketReceiver, PacketSender};
-use rand::Rng;
 use std::fs;
 use std::thread::sleep;
 use std::time::Duration;
-
-/// add padding at the beginning of the sequence,
-/// the padding section are filled with `pad_value`.
-/// make sure that `seq.len() % chunk_len == 0`
-fn add_padding<T: Clone>(seq: &mut Vec<T>, pad_value: T, chunk_len: usize) {
-  let padding_len = (chunk_len - seq.len() % chunk_len) % chunk_len;
-  seq.extend(std::iter::repeat(pad_value).take(padding_len));
-}
-/// remove the padding section in front of the original data.
-/// the length of the original sequence and the padding chunk len should be given.
-fn remove_padding<T: Clone>(seq: &mut Vec<T>, original_len: usize, chunk_len: usize) {
-  let _ = chunk_len;
-  seq.resize(original_len, seq[0].clone());
-}
-
-#[test]
-fn pad_add_remove() {
-  const TESTS: usize = 20;
-  for _ in 0..TESTS {
-    let mut rng = rand::thread_rng();
-    let data_len: usize = rng.gen_range(1000..2000);
-    let chunk_len: usize = rng.gen_range(1..10);
-
-    let pad_value: u8 = rng.gen();
-    let data: Vec<u8> = rng.sample_iter(rand::distributions::Standard).take(data_len).collect();
-    let mut data_pad = data.clone();
-    add_padding(&mut data_pad, pad_value, chunk_len);
-    remove_padding(&mut data_pad, data_len, chunk_len);
-    assert_eq!(data_pad, data)
-  }
-}
 
 const CHUNK_LEN: usize = PlainPHY::PACKET_BYTES;
 const DATA_LEN: usize = 10000 / 8;
