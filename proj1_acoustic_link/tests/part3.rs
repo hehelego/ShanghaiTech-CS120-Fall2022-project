b026d3e6b5a0b59c483ddafd1015ef1809ca3ea5
hehelego <2364261262@qq.com>
2022-10-10 12:09:46 +0800

    FIX: tests/part3.rs correct padding remove implementation

--- proj1_acoustic_link/tests/part3.rs
+++ proj1_acoustic_link/tests/part3.rs
@@ -1,5 +1,6 @@
 use proj1_acoustic_link::helper::bytes_to_bits;
 use proj1_acoustic_link::{helper::bits_to_bytes, phy_layer::PhyLayer};
+use rand::Rng;
 use std::fs;
 use std::thread::sleep;
 use std::time::Duration;
@@ -14,14 +15,30 @@ fn add_padding<T: Clone>(seq: &mut Vec<T>, pad_value: T, chunk_len: usize) {
 /// remove the padding section in front of the original data.
 /// the length of the original sequence and the padding chunk len should be given.
 fn remove_padding<T: Clone>(seq: &mut Vec<T>, original_len: usize, chunk_len: usize) {
-  let padding_len = (chunk_len - original_len % chunk_len) % chunk_len;
-  for i in 0..original_len {
-    seq[i] = seq[padding_len + i].clone();
-  }
+  let _ = chunk_len;
   seq.resize(original_len, seq[0].clone());
 }
 
+#[test]
+fn pad_add_remove() {
+  const TESTS: usize = 20;
+  for _ in 0..TESTS {
+    let mut rng = rand::thread_rng();
+    let data_len: usize = rng.gen_range(1000..2000);
+    let chunk_len: usize = rng.gen_range(1..10);
+
+    let pad_value: u8 = rng.gen();
+    let data: Vec<u8> = rng.sample_iter(rand::distributions::Standard).take(data_len).collect();
+    let mut data_pad = data.clone();
+    add_padding(&mut data_pad, pad_value, chunk_len);
+    remove_padding(&mut data_pad, data_len, chunk_len);
+    assert_eq!(data_pad, data)
+  }
+}
+
 const CHUNK_LEN: usize = PhyLayer::PACKET_BYTES;
+const DATA_LEN: usize = 10000 / 8;
+const PAD_LEN: usize = (CHUNK_LEN - DATA_LEN % CHUNK_LEN) % CHUNK_LEN;
 
 #[test]
 #[ignore]
@@ -55,8 +72,6 @@ fn part3_ck1_send() {
 #[ignore]
 fn part3_ck1_recv() {
   const FILEPATH: &str = "OUTPUT.txt";
-  const DATA_LEN: usize = 10000 / 8;
-  const PAD_LEN: usize = (CHUNK_LEN - DATA_LEN % CHUNK_LEN) % CHUNK_LEN;
   let mut physics_layer = PhyLayer::default();
 
   let mut bytes = vec![0; DATA_LEN + PAD_LEN];
