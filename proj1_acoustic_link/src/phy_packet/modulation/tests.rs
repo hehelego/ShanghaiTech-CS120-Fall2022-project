2ef48dbc8ed23c2b282512a35548452a76df33ae
hehelego <2364261262@qq.com>
2022-10-13 22:30:11 +0800

    UPD: `phy_packet/modulation/tests.rs` OFDM enc->WAV->OFDM dec test case

--- proj1_acoustic_link/src/phy_packet/modulation/tests.rs
+++ proj1_acoustic_link/src/phy_packet/modulation/tests.rs
@@ -65,3 +65,31 @@ fn ofdm_noise() {
     test_noisy(OFDM::new(), Standard);
   }
 }
+
+/// OFDM encode/decode + ideal transmission through WAV file
+#[test]
+fn ofdm_wav_once() {
+  use crate::sample_stream::{HoundInStream, HoundOutStream};
+  use crate::traits::{InStream, OutStream};
+
+  let mut codec = OFDM::new();
+  let bytes: Vec<u8> = rand::thread_rng()
+    .sample_iter(Standard)
+    .take(OFDM::BYTES_PER_PACKET * CODEC_TESTS)
+    .collect();
+
+  let mut out_stream = HoundOutStream::create("ofdm_test.wav");
+  bytes
+    .chunks_exact(OFDM::BYTES_PER_PACKET)
+    .for_each(|pack| out_stream.write_exact(&codec.encode(pack)).unwrap());
+  out_stream.finalize();
+
+  let mut in_stream = HoundInStream::open("ofdm_test.wav");
+  let mut received = vec![0.0; OFDM::SAMPLES_PER_PACKET * CODEC_TESTS];
+  in_stream.read_exact(&mut received).unwrap();
+  let decoded: Vec<_> = received
+    .chunks_exact(OFDM::SAMPLES_PER_PACKET)
+    .flat_map(|pack| codec.decode(pack))
+    .collect();
+  assert_eq!(bytes.as_slice(), decoded.as_slice());
+}
