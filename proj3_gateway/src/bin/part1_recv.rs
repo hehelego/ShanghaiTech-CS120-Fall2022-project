d2ee66b328582916ed8d74bc38db133af8d7fc4f
EAinsley <vhtmscyo@gmail.com>
2022-11-16 16:10:07 +0800

    ADD(part1_send, part1_recv): Finish proj3_part1

---
+++ proj3_gateway/src/bin/part1_recv.rs
@@ -0,0 +1,30 @@
+use clap::Parser;
+use std::net::UdpSocket;
+
+#[derive(Parser)]
+struct Cli {
+  /// host port
+  port: u16,
+}
+
+fn main() -> std::io::Result<()> {
+  const SELF_ADDR: &str = "127.0.0.1";
+  const DATA_SIZE: usize = 20;
+  const DATA_NUM: usize = 10;
+
+  let Cli { port } = Cli::parse();
+
+  let upd_socket = UdpSocket::bind((SELF_ADDR, port))?;
+  println!("Udp socket bind on {SELF_ADDR}:{port}");
+
+  let mut buffer = [0; DATA_SIZE];
+  let mut data_recv_count = 0;
+  while data_recv_count < DATA_NUM {
+    let (bytes, src) = upd_socket.recv_from(&mut buffer)?;
+    data_recv_count += 1;
+    println!("[{data_recv_count}] Receive {bytes} bytes data from {src}");
+  }
+
+  println!("Finish receiveing...exit.");
+  Ok(())
+}
---
+++ proj3_gateway/src/bin/part1_send.rs
@@ -0,0 +1,64 @@
+use clap::Parser;
+use std::{
+  net::UdpSocket,
+  thread,
+  time::{Duration, Instant},
+};
+
+use rand::{self, RngCore};
+
+#[derive(Parser)]
+struct Cli {
+  /// The host port
+  src_port: u16,
+  /// The ip address.
+  dest_addr: String,
+  /// The port
+  dest_port: u16,
+}
+
+fn main() -> std::io::Result<()> {
+  // Constants
+  const PAYLOAD_BYTES: usize = 20;
+  const LOOP_TIME: usize = 10;
+  const SEND_INTERVAL: Duration = Duration::from_secs(1);
+  const UDP_ADDR: &str = "127.0.0.1";
+
+  // Delay the start for 1 secs
+  thread::sleep(Duration::from_secs(1));
+  let program_start_time = Instant::now();
+
+  // Bind the socket
+  let Cli {
+    src_port,
+    dest_addr,
+    dest_port,
+  } = Cli::parse();
+  let node3_udp_socket = UdpSocket::bind((UDP_ADDR, src_port))?;
+  println!("Udp bind on: {UDP_ADDR}:{src_port}");
+
+  // Initialization
+  let mut random_payload = [0; PAYLOAD_BYTES];
+  let mut loop_time = 0;
+
+  while loop_time < LOOP_TIME {
+    let start_time = Instant::now();
+    // Fill the random byte
+    rand::thread_rng().fill_bytes(&mut random_payload);
+    // Send random data
+    node3_udp_socket.send_to(&random_payload, (dest_addr.as_str(), dest_port))?;
+    loop_time += 1;
+    let end_time = Instant::now();
+    // Information for the user.
+    println!(
+      "[{loop_time}] Send {PAYLOAD_BYTES} bytes to {dest_addr}:{dest_port} at {}s",
+      (end_time - program_start_time).as_secs_f32()
+    );
+    // Sleep for 1s.
+    if start_time.elapsed() < SEND_INTERVAL {
+      thread::sleep(SEND_INTERVAL - (start_time.elapsed()));
+    }
+  }
+  println!("Finish sending...exit");
+  Ok(())
+}
