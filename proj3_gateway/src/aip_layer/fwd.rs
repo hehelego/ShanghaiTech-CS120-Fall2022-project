f62a4421a93610594cab0c8d3f09ce6d3fcda1c7
hehelego <2364261262@qq.com>
2022-11-23 15:55:37 +0800

    fix(ip layer): internal node, reject bind on incorrect IP addr

--- proj3_gateway/src/aip_layer/fwd.rs
+++ proj3_gateway/src/aip_layer/fwd.rs
@@ -1,19 +1,17 @@
 use crate::{
+  aip_layer::ipc::{recv_packet, send_packet, IpcPath, Request, Response},
   common::{aip_ipc_sockaddr, AIP_SOCK, IPC_TIMEOUT},
   packet::{parse_udp, IpOverMac},
   ASockProtocol,
 };
 use pnet::packet::ipv4::Ipv4;
-use proj2_multiple_access::{MacAddr, MacLayer};
-use socket2::{Domain, SockAddr, Socket, Type};
+use proj2_multiple_access::MacAddr;
+use socket2::{Domain, Socket, Type};
 use std::{
   collections::HashMap,
   io::Result,
   net::{Ipv4Addr, SocketAddrV4},
-  path::Path,
 };
-
-use super::ipc::{recv_packet, send_packet, IpcPath, Request, Response, WrapIpv4};
 
 /// A unix-domain socket server which provides IP layer service:
 /// send IP packets to peer or receive IP packets from peer
@@ -44,6 +42,11 @@ pub struct IpLayerInternal {
 
 impl IpLayerInternal {
   fn on_recv_ipv4(&mut self, ipv4: Ipv4) {
+    // some thing wrong, receiving a packet not for us
+    if ipv4.destination != self.self_ip {
+      return;
+    }
+
     match ipv4.next_level_protocol.try_into() {
       Ok(ASockProtocol::UDP) => {
         parse_udp(&ipv4)
@@ -62,12 +65,19 @@ impl IpLayerInternal {
       _ => todo!(),
     };
   }
+  fn on_bind_failed(&self, ipc_path: IpcPath) {
+    let pack = Response::BindResult(false);
+    let _ = send_packet(&self.ipc, &ipc_path.as_sockaddr(), &pack);
+  }
   fn handle_bind(&mut self, protocol: ASockProtocol, addr: SocketAddrV4, ipc_path: IpcPath) {
+    if *addr.ip() != self.self_ip {
+      self.on_bind_failed(ipc_path);
+      return;
+    }
     match protocol {
       ASockProtocol::UDP => {
         if self.socks_in_use.get(&ipc_path).is_some() {
-          let pack = Response::BindResult(false);
-          let _ = send_packet(&self.ipc, &ipc_path.as_sockaddr(), &pack);
+          self.on_bind_failed(ipc_path);
         } else {
           self.socks_in_use.insert(ipc_path.clone(), (protocol, addr));
           self.udp_binds.insert(addr, ipc_path.clone());
