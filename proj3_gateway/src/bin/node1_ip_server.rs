3c2ca38cd360c0ff4df8f6dd3da9a28724bb086d
hehelego <2364261262@qq.com>
2022-11-24 09:27:56 +0800

    upd(ip layer): IP layer server will read command line arguments for IP address

--- proj3_gateway/src/bin/node1_ip_server.rs
+++ proj3_gateway/src/bin/node1_ip_server.rs
@@ -1,14 +1,37 @@
+use clap::Parser;
 use proj2_multiple_access::MacAddr;
 use proj3_gateway::IpLayerInternal;
-use std::net::Ipv4Addr;
+use std::{io::Result, net::Ipv4Addr};
 
-fn main() {
+/// IP layer server running on the internal node of Athernet (node1)
+#[derive(Parser)]
+struct AnetIpCli {
+  /// Athernet IP address of gateway node
+  gateway_ip: Ipv4Addr,
+  /// Athernet MAC address of gateway node
+  gateway_mac: u8,
+  /// Athernet IP address of internal node
+  internal_ip: Ipv4Addr,
+  /// Athernet MAC address of internal node
+  internal_mac: u8,
+}
+
+fn main() -> Result<()> {
   env_logger::init();
 
-  const SELF_MAC: MacAddr = MacAddr(1);
-  const SELF_IP: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 2);
-  const PEER_MAC: MacAddr = MacAddr(2);
-  const PEER_IP: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 1);
-  let mut ip_layer = IpLayerInternal::new((SELF_MAC, SELF_IP), (PEER_MAC, PEER_IP)).unwrap();
+  let AnetIpCli {
+    gateway_ip,
+    gateway_mac,
+    internal_ip,
+    internal_mac,
+  } = AnetIpCli::parse();
+  let gateway_mac = MacAddr(gateway_mac);
+  let internal_mac = MacAddr(internal_mac);
+
+  let gateway_addr = (gateway_mac, gateway_ip);
+  let internal_addr = (internal_mac, internal_ip);
+  let mut ip_layer = IpLayerInternal::new(internal_addr, gateway_addr)?;
   ip_layer.run();
+
+  Ok(())
 }
--- proj3_gateway/src/bin/node2_ip_server.rs
+++ proj3_gateway/src/bin/node2_ip_server.rs
@@ -1,14 +1,42 @@
 use proj2_multiple_access::MacAddr;
 use proj3_gateway::IpLayerGateway;
-use std::net::Ipv4Addr;
+use std::{io::Result, net::Ipv4Addr};
 
-fn main() {
+use clap::Parser;
+
+/// NAT server running on the gateway of Athernet (node2)
+#[derive(Parser)]
+struct NatCli {
+  /// Athernet IP address of gateway node
+  gateway_ip: Ipv4Addr,
+  /// Athernet MAC address of gateway node
+  gateway_mac: u8,
+  /// Athernet IP address of internal node
+  internal_ip: Ipv4Addr,
+  /// Athernet MAC address of internal node
+  internal_mac: u8,
+
+  /// Internet IP address of the gateway/NAT
+  nat_ip: Ipv4Addr,
+}
+
+fn main() -> Result<()> {
   env_logger::init();
 
-  const SELF_MAC: MacAddr = MacAddr(2);
-  const SELF_IP: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 1);
-  const PEER_MAC: MacAddr = MacAddr(1);
-  const PEER_IP: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 2);
-  let mut ip_layer = IpLayerGateway::new((SELF_MAC, SELF_IP), (PEER_MAC, PEER_IP)).unwrap();
+  let NatCli {
+    gateway_ip,
+    gateway_mac,
+    internal_ip,
+    internal_mac,
+    nat_ip,
+  } = NatCli::parse();
+  let gateway_mac = MacAddr(gateway_mac);
+  let internal_mac = MacAddr(internal_mac);
+
+  let gateway_addr = (gateway_mac, gateway_ip);
+  let internal_addr = (internal_mac, internal_ip);
+  let mut ip_layer = IpLayerGateway::new(gateway_addr, internal_addr, nat_ip)?;
   ip_layer.run();
+
+  Ok(())
 }
