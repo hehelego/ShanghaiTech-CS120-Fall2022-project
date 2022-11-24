55c8d2ac7c7b4093ee78071678044544cf31a6df
EAinsley <vhtmscyo@gmail.com>
2022-11-24 21:31:50 +0800

    RN(part2_send -> part2_send_node1): part2 send for node 1.

---
+++ proj3_gateway/src/bin/part2_send_node1.rs
@@ -0,0 +1,34 @@
+use clap::Parser;
+use proj3_gateway::UdpSocket;
+use std::{
+  fs::File,
+  io::{BufRead, BufReader},
+  net::{Ipv4Addr, SocketAddrV4},
+};
+
+#[derive(Parser)]
+struct Cli {
+  src_port: u16,
+  dest_addr: SocketAddrV4,
+}
+
+fn main() {
+  env_logger::init();
+  let Cli { src_port, dest_addr } = Cli::parse();
+  let self_addr: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(192, 168, 1, 2), src_port);
+  let upd_socket = UdpSocket::bind(self_addr).unwrap();
+  let data_lines = BufReader::new(File::open("INPUT.txt").unwrap()).lines();
+  let mut lines_count = 0;
+  println!("Starting send data to {dest_addr}");
+  for line in data_lines {
+    if let Ok(data) = line {
+      let data = data.as_bytes();
+      upd_socket.send_to(data, dest_addr).unwrap();
+      println!("Send {} bytes", data.len());
+      lines_count += 1;
+    }
+  }
+  upd_socket.send_to("".as_bytes(), dest_addr).unwrap();
+  upd_socket.send_to("".as_bytes(), dest_addr).unwrap();
+  println!("Send finished. {lines_count} lines in total");
+}
