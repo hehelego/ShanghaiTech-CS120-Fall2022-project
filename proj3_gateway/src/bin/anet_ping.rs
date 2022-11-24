2c2bc5c59bf1a24281bdc9c234e000201b30ea7b
hehelego <2364261262@qq.com>
2022-11-24 21:12:40 +0800

    upd(transport/icmp): ICMP socket and ping/pong utils
    ping

---
+++ proj3_gateway/src/bin/anet_ping.rs
@@ -0,0 +1,85 @@
+use clap::{Parser, Subcommand};
+use proj3_gateway::IcmpSocket;
+use std::io::{BufRead, BufReader, Error, ErrorKind, Result};
+use std::net::Ipv4Addr;
+use std::process;
+use std::time::Instant;
+
+const DEFAULT_PING_MSG: &str = "across the great wall we can reach the world";
+
+/// Send ICMP ping from Athernet node
+#[derive(Parser)]
+struct AnetPing {
+  #[command(subcommand)]
+  command: Ping,
+
+  #[arg(long, default_value_t = 10)]
+  /// the maximum number of ping-pong rounds
+  rounds: u32,
+
+  #[arg(long, default_value_t = String::from(DEFAULT_PING_MSG))]
+  /// ICMP echo request payload string
+  payload: String,
+}
+
+#[derive(Subcommand, Clone)]
+enum Ping {
+  /// Ping to a host specified by IPv4 address
+  Direct {
+    /// Destination IPv4 address
+    ip: Ipv4Addr,
+  },
+  /// Ping to a host specified by host name
+  Dns {
+    /// Destination host name
+    host: String,
+  },
+}
+
+fn dns_resolve(host: String) -> Result<Ipv4Addr> {
+  let stdout = process::Command::new("dig").arg("+short").arg(host).output()?.stdout;
+
+  let mut reader = BufReader::new(&stdout[..]);
+  let mut addr = String::new();
+  reader.read_line(&mut addr)?;
+
+  addr.trim().parse().map_err(|e| Error::new(ErrorKind::NotFound, e))
+}
+
+fn main() -> Result<()> {
+  env_logger::init();
+
+  let AnetPing {
+    command: dest,
+    rounds,
+    payload,
+  } = AnetPing::parse();
+
+  let dest = match dest {
+    Ping::Direct { ip } => ip,
+    Ping::Dns { host } => dns_resolve(host).expect("DNS resolve failed"),
+  };
+  println!("PING {:?} with: {}", dest, payload);
+
+  let sock = IcmpSocket::bind(Ipv4Addr::new(192, 168, 1, 2)).expect("cannot create ICMP socket");
+
+  let id = rand::random();
+  let mut total_time = 0;
+  for seq in (0..rounds).map(|x| x as u16) {
+    let start = Instant::now();
+    sock.send_ping(id, seq, payload.as_bytes(), dest).unwrap();
+    let reply_payload = sock.recv_pong(id, seq, dest).unwrap();
+    let rtt = start.elapsed().as_millis() as u32;
+    total_time += rtt;
+    println!(
+      "REPLY(seq={}) from {:?} RTT={}ms with: {:?}",
+      seq,
+      dest,
+      rtt,
+      String::from_utf8(reply_payload),
+    );
+  }
+  println!("average RTT of {} packets is {}", rounds, total_time / rounds);
+
+  Ok(())
+}
--- proj3_gateway/src/socket/udp.rs
+++ proj3_gateway/src/socket/udp.rs
@@ -1,7 +1,7 @@
 use super::ASockProtocol;
 use crate::aip_layer::IpAccessor;
 use pnet::packet::udp::Udp;
-use std::net::SocketAddrV4;
+use std::{io::Result, net::SocketAddrV4};
 
 /// A socket for sending/receiving UDP packets.
 /// Provides transport layer APIs.
@@ -13,13 +13,13 @@ pub struct UdpSocket {
 impl UdpSocket {
   pub const PROTOCOL: ASockProtocol = ASockProtocol::UDP;
   pub const MAX_PAYLOAD_LEN: usize = u16::MAX as usize;
-  pub fn bind(addr: SocketAddrV4) -> std::io::Result<Self> {
+  pub fn bind(addr: SocketAddrV4) -> Result<Self> {
     let path = format!("udp_client_{}", addr);
     let accessor = IpAccessor::new(&path)?;
     accessor.bind(Self::PROTOCOL, addr)?;
     Ok(Self { accessor, addr })
   }
-  pub fn send_to(&self, buf: &[u8], addr: SocketAddrV4) -> std::io::Result<usize> {
+  pub fn send_to(&self, buf: &[u8], addr: SocketAddrV4) -> Result<usize> {
     let bytes_written = Self::MAX_PAYLOAD_LEN.min(buf.len());
     let udp_packet = Udp {
       source: self.addr.port(),
@@ -31,7 +31,7 @@ impl UdpSocket {
     self.accessor.send_udp(udp_packet, addr)?;
     Ok(bytes_written)
   }
-  pub fn recv_from(&self, buf: &mut [u8]) -> std::io::Result<(usize, SocketAddrV4)> {
+  pub fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddrV4)> {
     let (Udp { payload, length, .. }, addr) = self.accessor.recv_udp()?;
     buf[..length as usize].clone_from_slice(&payload);
     Ok((length as usize, addr))
