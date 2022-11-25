3555f725152bc608f43fb55c78ec7cd7c1e39051
EAinsley <vhtmscyo@gmail.com>
2022-11-25 22:39:00 +0800

    UPD(tcp.rs): finish TcpStream.

--- proj3_gateway/src/socket/tcp.rs
+++ proj3_gateway/src/socket/tcp.rs
@@ -1,6 +1,9 @@
 use crossbeam_channel::{unbounded, Receiver, Sender};
 use pnet::packet::tcp::Tcp;
-use std::{net::SocketAddrV4, time::Duration};
+use std::{
+  net::SocketAddrV4,
+  time::{Duration, Instant},
+};
 
 use super::ASockProtocol;
 
@@ -19,38 +22,114 @@ enum TcpState {
   Closed,
 }
 
-struct TcpStateMachine {
+pub(crate) struct TcpStateMachine {
   state: TcpState,
-  packet_assembled: Sender<u8>,
+  bytes_assembled: Sender<u8>,
   packet_received: Receiver<Tcp>,
   bytes_to_send: Receiver<u8>,
 }
 
-pub struct TcpStream;
+impl TcpStateMachine {
+  pub fn new(bytes_assembled: Sender<u8>, bytes_to_send: Receiver<u8>, addr: SocketAddrV4) -> Self {
+    todo!()
+  }
+  pub fn connect(&self) {
+    todo!()
+  }
+  pub fn sync(&self, dest: SocketAddrV4) -> Result<(), ()> {
+    todo!()
+  }
+  pub fn shutdown(&self) -> Result<(), ()> {
+    todo!()
+  }
+}
+
+pub struct TcpStream {
+  bytes_to_send: Sender<u8>,
+  bytes_assembled: Receiver<u8>,
+  state_machine: TcpStateMachine,
+}
 
 impl TcpStream {
   pub const PROTOCOL: ASockProtocol = ASockProtocol::TCP;
 
   pub fn bind(addr: SocketAddrV4) -> Result<Self, ()> {
-    todo!()
+    let (bytes_assembled_tx, bytes_assmebled_rx) = crossbeam_channel::unbounded();
+    let (bytes_to_send_tx, bytes_to_send_rx) = crossbeam_channel::unbounded();
+    // Create the Tcp State Machine
+    let state_machine = TcpStateMachine::new(bytes_assembled_tx, bytes_to_send_rx, addr);
+    // Tcp StateMachine
+    Ok(Self {
+      bytes_to_send: bytes_to_send_tx,
+      bytes_assembled: bytes_assmebled_rx,
+      state_machine,
+    })
   }
 
-  pub(crate) fn transfer() -> Self {
-    todo!()
+  pub(crate) fn transfer(
+    bytes_to_send: Sender<u8>,
+    bytes_assembled: Receiver<u8>,
+    state_machine: TcpStateMachine,
+  ) -> Self {
+    Self {
+      bytes_to_send,
+      bytes_assembled,
+      state_machine,
+    }
   }
 
-  pub fn connect(&self, dest: SocketAddrV4) -> Result<(), ()> {
-    todo!()
+  pub fn connect(&mut self, dest: SocketAddrV4) -> Result<(), ()> {
+    self.state_machine.sync(dest)
   }
 
   pub fn shutdown(&self) -> Result<(), ()> {
-    todo!()
+    self.state_machine.shutdown()
   }
   pub fn read_timeout(&self, buf: &mut [u8], timeout: Option<Duration>) -> Result<usize, ()> {
-    todo!()
+    let mut bytes_read = 0;
+    if let Some(timeout) = timeout {
+      let deadline = Instant::now() + timeout;
+      for x in buf.iter_mut() {
+        match self.bytes_assembled.recv_deadline(deadline) {
+          Ok(byte) => {
+            bytes_read += 1;
+            *x = byte
+          }
+          Err(_) => return Ok(bytes_read),
+        }
+      }
+    } else {
+      for x in buf.iter_mut() {
+        match self.bytes_assembled.recv() {
+          Ok(byte) => {
+            *x = byte;
+            bytes_read += 1;
+          }
+          Err(_) => return Ok(bytes_read),
+        }
+      }
+    }
+    Ok(bytes_read)
   }
   pub fn write_timeout(&self, buf: &[u8], timeout: Option<Duration>) -> Result<usize, ()> {
-    todo!()
+    let mut byte_writes = 0;
+    if let Some(timeout) = timeout {
+      let deadline = Instant::now() + timeout;
+      for x in buf.iter() {
+        match self.bytes_to_send.send_deadline(*x, deadline) {
+          Ok(_) => byte_writes += 1,
+          Err(_) => return Ok(byte_writes),
+        }
+      }
+    } else {
+      for x in buf.iter() {
+        match self.bytes_to_send.send(*x) {
+          Ok(_) => byte_writes += 1,
+          Err(_) => return Ok(byte_writes),
+        }
+      }
+    }
+    Ok(byte_writes)
   }
 }
 
