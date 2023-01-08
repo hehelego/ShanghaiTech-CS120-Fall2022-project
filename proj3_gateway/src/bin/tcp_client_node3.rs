6221be12c7e2d60419a636034c0698497d20857b
EAinsley <vhtmscyo@gmail.com>
2023-01-08 22:22:37 +0800

    ADD(tcp_client_node3): simple test for tcp stream on node 3 as client.

---
+++ proj3_gateway/src/bin/tcp_client_node3.rs
@@ -0,0 +1,29 @@
+use clap::Parser;
+use std::net::TcpStream;
+
+use std::io::{Read, Write};
+#[derive(Parser)]
+struct Cli {
+  /// The ip address.
+  dest_addr: String,
+  /// The port
+  dest_port: u16,
+}
+
+fn main() -> std::io::Result<()> {
+  // Bind the socket
+  let Cli { dest_addr, dest_port } = Cli::parse();
+  let mut tcp_stream = TcpStream::connect((dest_addr.clone(), dest_port))?;
+  println!(
+    "Tcp connect to {dest_addr}:{dest_port}, host port: {}",
+    tcp_stream.local_addr().unwrap()
+  );
+
+  // Read data
+  let data_lines = std::fs::read("INPUT.txt").unwrap();
+  println!("Starting send data to {dest_addr}:{dest_port}");
+  tcp_stream.write_all(&data_lines).unwrap();
+  println!("Data sent");
+  tcp_stream.shutdown(std::net::Shutdown::Write)?;
+  Ok(())
+}
