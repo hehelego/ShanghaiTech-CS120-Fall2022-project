c37a65a1f2ee3e2cf06fbb3407229ace6417169f
hehelego <2364261262@qq.com>
2023-01-12 18:55:31 +0800

    upd(proj4): initialize proj4-ftp crate structure

--- proj4_ftp/src/lib.rs
+++ proj4_ftp/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-  left + right
-}
+/// Wrap of Athernet TCP socket
+mod asock;
+
+/// SOCKS5 protocol proxy server that
+/// forwards normal network traffic through Athernet.
+mod proxy_server;
+
+/// FTP client built on Athernet TCP transport layer.
+mod ftp_client;
 
 #[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
-  }
-}
+mod tests;
