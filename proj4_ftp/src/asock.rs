6b3147b8d624733ea7bbb41bfcb352acd273a168
hehelego <2364261262@qq.com>
2023-01-14 22:46:22 +0800

    fix(ftp/asock): enable clone with Rc, correct Read

--- proj4_ftp/src/asock.rs
+++ proj4_ftp/src/asock.rs
@@ -1,27 +1,33 @@
 use proj3_gateway::TcpStream;
 use std::io::{Error, ErrorKind, Read, Result, Write};
 use std::net::{Shutdown, SocketAddrV4};
+use std::rc::Rc;
 use std::time::Duration;
 
 const ASOCK_TIMEOUT: Duration = Duration::from_secs(2);
 const ASOCK_FLUSH_TIME: Duration = Duration::from_millis(50);
+
 /// Wrapper for Athernet TCP socket, provide Reader/Writer implementation
-pub struct ASocket(TcpStream);
+#[derive(Clone)]
+pub struct ASocket(Rc<TcpStream>);
 
 impl ASocket {
   pub fn bind(addr: SocketAddrV4) -> Result<ASocket> {
     TcpStream::bind(addr)
-      .map(ASocket)
+      .map(|sock| ASocket(Rc::new(sock)))
       .map_err(|_| Error::new(ErrorKind::AddrInUse, "address in use, bind failed".to_string()))
   }
 
   pub fn connect(&mut self, dest: SocketAddrV4) -> Result<()> {
-    self.0.connect(dest).map_err(|_| {
-      Error::new(
-        ErrorKind::ConnectionRefused,
-        "cannot connect to remote host".to_string(),
-      )
-    })
+    Rc::get_mut(&mut self.0)
+      .expect("Athernet Tcp Socket borrow mut")
+      .connect(dest)
+      .map_err(|_| {
+        Error::new(
+          ErrorKind::ConnectionRefused,
+          "cannot connect to remote host".to_string(),
+        )
+      })
   }
 
   pub fn shutdown(&mut self, how: Shutdown) -> Result<()> {
@@ -37,13 +43,12 @@ impl ASocket {
 impl Read for ASocket {
   fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
     let (n, fin) = self.0.read_timeout(buf, Some(ASOCK_TIMEOUT));
-    if n == 0 && fin {
-      Err(Error::new(
-        ErrorKind::ConnectionAborted,
-        "connection cloesd by remote".to_string(),
-      ))
-    } else {
+    if n > 0 {
       Ok(n)
+    } else if !fin {
+      Err(Error::new(ErrorKind::Interrupted, "wait for incoming data".to_string()))
+    } else {
+      Ok(0)
     }
   }
 }
